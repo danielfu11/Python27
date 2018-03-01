1e9ff679bea234c539e9201b4985fa363e58bb90
Guanxiong Fu <gufu3017@colorado.edu>
2018-02-28 18:55:53 -0700

    Learned printing syntax lol

---
+++ practice1.py
@@ -0,0 +1,16 @@
+# practice printing syntax
+print "Hello World!"
+print 'Hello World!'
+print "'Hello World'"
+print '"Hello World"'
+print "two plus two is:", 2+2
+print "minus one is:", 2+2-1
+print "remainder is:", 2+2-1 %2
+print "or is it:", (2+2-1)%2
+print "is 2+2 greater than 2+2-1:", 2+2 > 2+2-1
+print "or:", (2+2)>(2+2-1)
+print "are they equal:", 2+2 == 2+2-1
+print "7 divide by 4:", 7/4
+print "7.0 divide by 4.0:", 7.0/4.0
+print "7 divide by 3:", 7/3
+print "Hey %s there" % "you"
